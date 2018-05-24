3def0a73815a3c61351f9e14ef9c87a36e8ab2c5
jpaciello <juliopaciello@gmail.com>
2018-05-24 16:55:45 -0400

    Se modifico el script para traer todos los datos, no solo 2016/08

--- practicas/DM/prediccion/obligaciones_pagos_contratos.sql
+++ practicas/DM/prediccion/obligaciones_pagos_contratos.sql
@@ -1,17 +1,25 @@
 ﻿
 select fo.fecha_factura, fot.fecha_deposito, dp.id as proveedor_id,
-de.entidad_codigo, dog.objeto_gasto_codigo
+de.entidad_codigo, dog.objeto_gasto_codigo, 
+(dtdeposito.dayindimension::int-dtfactura.dayindimension::int) as diff
 from fact_obligacion fo, fact_ot fot, dim_junk_obligacion djo,
 v_contrato vo, fact_contrato fc, dim_proveedor dp,
-dim_clasificador_entidad de, dim_clasificador_objeto_gasto dog
+dim_clasificador_entidad de, dim_clasificador_objeto_gasto dog,
+dim_tiempo dtfactura, dim_tiempo dtdeposito,
+dim_tiempo dtob, dim_tiempo dtot
 where fo.numero_str = fot.numero_str
-and fo.fecha_aprobacion between 20160101 and 20161231
-and fot.fecha_deposito between 20160101 and 20161231
+and fo.fecha_aprobacion = dtob.id
+and fot.fecha_deposito = dtot.id
+and dtob.year = dtot.year 
+and dtob.month = dtot.month
 and fo.junk_obligacion = djo.id
 and vo.codigo_contratacion = djo.codigo_contratacion
 and vo.contrato_id = fc.id
 and dp.id = fc.proveedor
 and fo.clasificador_entidad = de.id
 and fo.clasificador_objeto_gasto = dog.id
---and fo.numero_str = 93641
-
+and dtfactura.id = fo.fecha_factura
+and dtdeposito.id = fot.fecha_deposito
+and dtfactura.id > 0
+and dtdeposito.id > 0
+order by diff
