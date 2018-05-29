a8b55be2995def7584788f99c0f3dfbefd2504ee
jpaciello <juliopaciello@gmail.com>
2018-05-29 15:26:11 -0400

    Se agrega el workflow KNIME de clustering y un sql que trae montos adjudicados

---
+++ practicas/DM/prediccion/obligaciones_pagos_contratos_adjudicaciones.sql
@@ -0,0 +1,30 @@
+﻿
+select fo.fecha_factura, fot.fecha_deposito, dp.id as proveedor_id,
+	de.id as entidad_id, dog.objeto_gasto_codigo, 
+	(dtdeposito.dayindimension::int-dtfactura.dayindimension::int) as diff,
+	de.entidad_descripcion,dog.objeto_gasto_descripcion,dp.razon_social,
+	fc.monto_adjudicado, dtp.tipo_procedimiento_abreviatura
+
+from fact_obligacion fo, fact_ot fot, dim_junk_obligacion djo,
+	v_contrato vo, fact_contrato fc, dim_proveedor dp,
+	dim_clasificador_entidad de, dim_clasificador_objeto_gasto dog,
+	dim_tiempo dtfactura, dim_tiempo dtdeposito,
+	dim_tiempo dtob, dim_tiempo dtot, dim_tipo_procedimiento_dncp dtp
+
+where fo.numero_str = fot.numero_str
+	and fo.fecha_aprobacion = dtob.id
+	and fot.fecha_generacion = dtot.id
+	and dtob.year = dtot.year 
+	and dtob.month = dtot.month
+	and fo.junk_obligacion = djo.id
+	and vo.codigo_contratacion = djo.codigo_contratacion
+	and vo.contrato_id = fc.id
+	and dp.id = fc.proveedor
+	and fo.clasificador_entidad = de.id
+	and fo.clasificador_objeto_gasto = dog.id
+	and dtfactura.id = fo.fecha_factura
+	and dtdeposito.id = fot.fecha_deposito
+	and fc.tipo_procedimiento_dncp = dtp.id
+	and dtfactura.id > 0
+	and dtdeposito.id > 0
+order by diff
