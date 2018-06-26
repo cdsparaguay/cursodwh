15efe0e185e223eb53e3c6cec35061b29fe2e0c0
Hector Martinez <hmartinez.py@gmail.com>
2018-06-25 22:55:04 -0400

    Agrega estructura para proveedores en esquema ODS

---
+++ practicas/querys/ods/dncp-7-proveedores-estructura.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE ods.dncp_proveedores
+(
+	cantidad_adjudicaciones character varying
+	, direccion character varying
+	, email character varying
+	, id character varying
+	, nombre_fantasia character varying
+	, razon_social character varying
+	, representante_legal character varying
+	, ruc character varying
+	, telefono character varying
+	, checksum character varying
+)
